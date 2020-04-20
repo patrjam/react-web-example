9d33c440a125de4b08a2bbe4cf20440acc1a1f46
jamrip01 <patricia.jamriskova01@dixonscarphone.com>
2020-04-20 11:20:57 +0200

    Updates

---
+++ precache-manifest.75bb7d2808a320745b3331c98044856a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca8d0dc8a49ccfa11b6d565010971a2a",
+    "url": "/react-web-example/index.html"
+  },
+  {
+    "revision": "d25a88369e22e0963d2c",
+    "url": "/react-web-example/static/css/main.56ad937f.chunk.css"
+  },
+  {
+    "revision": "04cb6c7fe8847fa178cf",
+    "url": "/react-web-example/static/js/2.a4bd6436.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-web-example/static/js/2.a4bd6436.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d25a88369e22e0963d2c",
+    "url": "/react-web-example/static/js/main.3e21ade2.chunk.js"
+  },
+  {
+    "revision": "b775776e27211ec04a20",
+    "url": "/react-web-example/static/js/runtime-main.a1c7be49.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-web-example/static/media/logo.5d5d9eef.svg"
+  }
+]);
